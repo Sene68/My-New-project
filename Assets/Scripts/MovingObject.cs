eaaf345c1b309b7552276ecbc2b4622c7888396e
Sene68 <senefull68@gmail.com>
2023-06-26 20:31:13 +0900

    shift 클릭 시 달리기

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -8,6 +8,9 @@ public class MovingObject : MonoBehaviour
     public float speed;
     private Vector3 vector;
 
+    public float runSpeed;
+    private float applyRunSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +21,19 @@ public class MovingObject : MonoBehaviour
     void Update()
     {
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
+
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                applyRunSpeed = runSpeed;
+            } else {
+                applyRunSpeed = 0;
+            }
+
             vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
 
             if (vector.x != 0) {
-                transform.Translate(vector.x * speed, 0, 0);
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
             } else if(vector.y != 0) {
-                transform.Translate(0, vector.y * speed, 0);
+                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
             }
         }
     }
